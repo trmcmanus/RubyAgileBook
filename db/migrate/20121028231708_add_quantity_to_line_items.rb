99ed293fc5b17c7ddd0ca9edf3881d5422c1d88f
Thomas McManus <tmcmanus@ymail.com>
2012-10-29 00:06:55 +0000

    Depot Cart2

--- db/migrate/20121028231708_add_quantity_to_line_items.rb
+++ db/migrate/20121028231708_add_quantity_to_line_items.rb
@@ -6,16 +6,8 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class Cart < ActiveRecord::Base
-  has_many :line_items, dependent: :destroy
-
-  def add_product(product_id)
-    current_item = line_items.find_by_product_id(product_id)
-    if current_item
-      current_item.quantity += 1
-    else
-      current_item = line_items.build(product_id: product_id)
-    end
-    current_item
+class AddQuantityToLineItems < ActiveRecord::Migration
+  def change
+    add_column :line_items, :quantity, :integer, default: 1
   end
 end
